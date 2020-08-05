da803a3fb7935462dbd60b80e075faae0ef28bd2
Zach Stearman <zstearman3@gmail.com>
2020-08-05 10:30:32 -0500

    Add Intro component

---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render() {
+    return (
+      <h1 className="intro-header">Hi, my name is Zach Stearman, and I am a software developer.</h1>
+    );
+  }
+}
+
+export default Intro;
